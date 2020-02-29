97aaabfb67c826de3d22217e39fea0c81cdcc18d
Felix Kleine Bösing <felix.boesing@t-online.de>
2020-02-29 22:29:18 +0100

    add optimizing method to calculate the number of agents.

--- src/modelling/erlang/erlangcp.py
+++ src/modelling/erlang/erlangcp.py
@@ -1,5 +1,8 @@
 from scipy.stats import gamma
+from scipy.optimize import minimize, minimize_scalar
+
 from src.modelling.helpers import power_faculty
+
 
 
 class ErlangCP:
@@ -84,6 +87,21 @@ class ErlangCP:
         return self.get_mean_number_customer_in_system(lambda_=lambda_, mu=mu, nu=nu, number_agents=number_agents,
                                                        size_waiting_room=size_waiting_room) / lambda_
 
+    def get_number_agents_for_service_level(self, lambda_, mu: float, nu: float, max_waiting_time: int,
+                                            size_waiting_room: int = None, target_sla: float = 0.8):
+        def func(self, lambda_, mu, nu, max_waiting_time, size_waiting_room, target_sla):
+            def wrapper(number_agents):
+                res = (target_sla - self.get_max_waiting_probability(lambda_=lambda_, mu=mu, nu=nu,
+                                                                     number_agents=int(number_agents),
+                                                                     max_waiting_time=max_waiting_time,
+                                                                     size_waiting_room=size_waiting_room)) ** 2
+                return res
+            return wrapper
+
+        result = minimize_scalar(func(self, lambda_, mu, nu, max_waiting_time, size_waiting_room, target_sla))
+        print(result)
+        return result
+
 
 def get_prob_for_pn_in_mmckm_system(lambda_: float, mu: float, nu: float, number_agents: int, size_waiting_room: int,
                                     persons_in_system: int):
@@ -118,6 +136,8 @@ if __name__ == "__main__":
     erlang = ErlangCP()
     print(erlang.get_max_waiting_probability(lambda_=1/10, mu=1/240, nu=1/300, number_agents=28, size_waiting_room=80,
                                              max_waiting_time=20))
+    print(erlang.get_number_agents_for_service_level(lambda_=1/10, mu=1/240, nu=1/300, target_sla=0.8633721956843062,
+                                                     size_waiting_room=80, max_waiting_time=20))
     print(erlang.get_prob_for_abort(lambda_=1/10, mu=1/240, nu=1/30, number_agents=25, size_waiting_room=80))
     print(erlang.get_mean_queue_length(lambda_=1/10, mu=1/240, nu=1/300, number_agents=28, size_waiting_room=80))
     print(erlang.get_mean_number_customer_in_system(lambda_=1/10, mu=1/240, nu=1/300, number_agents=28,
