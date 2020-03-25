90191070bd3cf5d333918ead48ae4547353286ae
Felix Kleine Bösing <felix.boesing@t-online.de>
2020-03-25 10:35:07 +0100

    update coverage bash file

--- misc/run_coverage.sh
+++ misc/run_coverage.sh
@@ -1,4 +1,12 @@
 cd ./tests
-coverage run -m --source="../src" pytest
+test_output="$(coverage run -m --source="../src" pytest)"
+tail_output=$(echo "$test_output" | tail -n -1)
 coverage report -m
 cd ../
+
+if [["$tail_output" =~ failed]] || [[ "$tail_output" =~ error ]]; then
+    echo "At least one test failed"
+    exit 1
+fi
+
+echo "All tests were passed"
