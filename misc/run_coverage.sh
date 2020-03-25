0d3d229e3f4133afd1a5475db5f7278d3698c366
Felix Kleine Bösing <felix.boesing@t-online.de>
2020-03-25 11:01:01 +0100

    fix run_coverage.sh

--- misc/run_coverage.sh
+++ misc/run_coverage.sh
@@ -4,7 +4,7 @@ tail_output=$(echo "$test_output" | tail -n -1)
 coverage report -m
 cd ../
 
-if [["$tail_output" =~ failed]] || [[ "$tail_output" =~ error ]]; then
+if [[ "$tail_output" =~ failed ]] || [[ "$tail_output" =~ error ]]; then
     echo "At least one test failed"
     exit 1
 fi
