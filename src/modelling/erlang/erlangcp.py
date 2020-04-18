b352b00ff8850e439e2662adc47257693a74372f
Felix Kleine Bösing <felix.boesing@t-online.de>
2020-04-18 22:05:49 +0200

    finalized OPtimizer which is able to find estimate a parameter for each function based on the other parameters

--- src/modelling/erlang/erlangcp.py
+++ src/modelling/erlang/erlangcp.py
@@ -153,31 +153,6 @@ class ErlangCP(Optimizer):
         return self.get_mean_number_customer_in_system(lambda_=lambda_, mu=mu, nu=nu, number_agents=number_agents,
                                                        size_waiting_room=size_waiting_room) / lambda_
 
-    def get_number_agents_for_service_level(self, lambda_, mu: float, nu: float, max_waiting_time: int,
-                                            size_waiting_room: int = None, target_sla: float = 0.8):
-        """
-        calculates the number of agents that are necessary to achieve a desired service level
-
-        :param lambda_: average arrival time in times per second
-        :param mu: average supply time in times per second
-        :param nu: abort rate in 1/sec (waiting tolerance of 60 sec = 1/ 60 == 0.01666
-        :param max_waiting_time: maximum waiting time until customers leave
-        :param size_waiting_room: size of a waiting room. If None it is infinite
-        :param target_sla: the sla level that you want to achieve
-        :return:
-        """
-
-        def optim_func(number_agents):
-            res = (target_sla - self.get_max_waiting_probability(lambda_=lambda_, mu=mu, nu=nu,
-                                                                 number_agents=int(number_agents),
-                                                                 max_waiting_time=max_waiting_time,
-                                                                 size_waiting_room=size_waiting_room)) ** 2
-            return res
-
-        result = minimize_scalar(optim_func)
-
-        return int(result.x) + 1
-
 
 def get_prob_for_pn_in_mmckm_system(lambda_: float, mu: float, nu: float, number_agents: int, size_waiting_room: int,
                                     persons_in_system: int):
@@ -241,7 +216,7 @@ if __name__ == "__main__":
                                    size_waiting_room=80, max_waiting_time=20))
     print(erlang.minimize(method=erlang.get_max_waiting_probability, kwargs=dict(lambda_=1/10, mu=1/240, nu=1/300,
                                                                                  size_waiting_room=80, max_waiting_time=20),
-                          optim_argument="number_agents", target_value=0.8633721956843062, optim_argument_type=int))
+                          optim_argument="number_agents", target_value=0.8633721956843062))
     print(erlang.get_prob_for_abort(lambda_=1/10, mu=1/240, nu=1/30, number_agents=25, size_waiting_room=80))
     print(erlang.get_mean_queue_length(lambda_=1/10, mu=1/240, nu=1/300, number_agents=28, size_waiting_room=80))
     print(erlang.get_mean_number_customer_in_system(lambda_=1/10, mu=1/240, nu=1/300, number_agents=28,
--- src/modelling/erlang/optimizer.py
+++ src/modelling/erlang/optimizer.py
@@ -10,8 +10,7 @@ class Optimizer:
     def __init__(self):
         pass
 
-    def minimize(self, method: MethodType, kwargs: dict, optim_argument: str, target_value: Union[float, int],
-                 optim_argument_type: type = float):
+    def minimize(self, method: MethodType, kwargs: dict, optim_argument: str, target_value: Union[float, int]):
         """
         minimizes the squared error of any given method with given parameters and one optim_argument which
         should be estimated
@@ -27,13 +26,18 @@ class Optimizer:
         assert isinstance(kwargs, dict), "kwargs must be of type dictionary"
         assert isinstance(optim_argument, str), "optim_argument must be of type str"
         assert isinstance(target_value, (float, int)), "target value must be integer or float"
-        assert isinstance(optim_argument_type, type)
-        inspect.getfullargspec(method)
-        # TODO inspect which datatype is annotated for given optimi_argument
+        func_inspect = inspect.getfullargspec(method)
+        args = func_inspect.args
+        annotations = func_inspect.annotations
+        assert optim_argument in args, "The optim_argument '{}' is not an argument of the specified function!".format(optim_argument)
+        assert all([arg in args for arg in kwargs.keys()]), "One of the supplied arguments is not present inside the "
+        assert all([isinstance(value, annotations[key]) for key, value in kwargs.items()]), \
+            "One of the supplied arguments has the wrong return type!"
+        target_type = annotations[optim_argument]
         kwargs = copy.deepcopy(kwargs)
 
         def optim_func(x):
-            kwargs[optim_argument] = optim_argument_type(x)
+            kwargs[optim_argument] = target_type(x)
             res = (target_value - method(**kwargs)) ** 2
             return res
 
