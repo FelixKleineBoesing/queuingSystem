63fd076c4551e210a0473d7dd64a59e7203add2b
Felix Kleine Bösing <felix.boesing@t-online.de>
2020-07-05 20:34:32 +0200

    add backoffice

--- src/controller/capacity_planning_backoffice.py
+++ src/controller/capacity_planning_backoffice.py
@@ -6,4 +6,13 @@ from src.misc.helper_functions import annotation_type_checker
 
 
 class BackOfficeController:
-    pass
+
+    @annotation_type_checker
+    @check_length_list_equality
+    def get_number_agents(self):
+        pass
+
+    @annotation_type_checker
+    @check_length_list_equality
+    def get_volume(self):
+        pass
---
+++ src/modelling/capacity_planning/backoffice/backoffice.py
@@ -0,0 +1,7 @@
+
+
+
+class BackOfficeCalculator:
+
+    def get_required_agents(self, incoming_value: list, open_index: int, closed_index: int, ):
+        pass
