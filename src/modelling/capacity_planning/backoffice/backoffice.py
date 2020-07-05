40bfecdf12e3e81ae4eec8b54b3263b18e254ee4
Felix Kleine Bösing <felix.boesing@t-online.de>
2020-07-05 21:31:42 +0200

    add asserts

--- src/modelling/capacity_planning/backoffice/backoffice.py
+++ src/modelling/capacity_planning/backoffice/backoffice.py
@@ -1,7 +1,13 @@
 
 
-
 class BackOfficeCalculator:
 
-    def get_required_agents(self, incoming_value: list, open_index: int, closed_index: int, ):
+    def get_required_agents(self, lambdas: list, mus: list, open_index: int, closed_index: int,
+                            backlog_within: int, occupancy: float):
+        assert len(lambdas) == len(mus)
+        assert closed_index < len(lambdas)
+        assert open_index < len(lambdas)
+        assert open_index < closed_index
+        assert closed_index - open_index + 1 == len(mus), "mus must be as long as the interval from open to closed"
+        
         pass
