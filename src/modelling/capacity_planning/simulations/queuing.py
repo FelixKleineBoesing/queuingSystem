ec6da002cdbf0b2d5835da4e209be0b45599109a
Felix Kleine Bösing <felix.boesing@t-online.de>
2020-07-24 00:35:20 +0200

    minor update

--- src/modelling/capacity_planning/simulations/queuing.py
+++ src/modelling/capacity_planning/simulations/queuing.py
@@ -1,4 +1,5 @@
 from sortedcontainers import SortedList
+from sortedcontainers import SortedDict
 from typing import List
 import numpy as np
 from aenum import Enum
@@ -6,16 +7,20 @@ from aenum import Enum
 
 class Customer:
 
-    def __init__(self, patience: int, channel: str, language: str = None, retrial: bool = False):
+    def __init__(self, patience: int, channel: str, language: str = None, retrial: bool = False, id: int = 0):
         """
 
         :param patience: the patience of this customer
+        :param channel
+        :param language
         :param retrial: whether he would retrial if he abandons
+        :param id:
         """
         self.patience = patience
         self.retrial = retrial
         self.channel = channel
         self.language = language
+        self.id = id
 
 
 class Process:
@@ -38,7 +43,8 @@ class Process:
 
         :return: a tuple of the time that this customers appears since the last customer
         """
-        return 1, None
+        return 1.0, Customer(patience=1, channel=self.channel, language=self.language,
+                           retrial=True)
 
 
 class Worker:
@@ -56,9 +62,8 @@ class Worker:
 
 class Event:
 
-    def __init__(self, event_type, appearance_time, func, **kwargs):
+    def __init__(self, event_type, appearance_time, **kwargs):
         self.event_type = event_type
-        self.func = func
         self.kwargs = kwargs
         self.appearance_time = appearance_time
 
@@ -98,7 +103,12 @@ class System:
         self.customer_queue_abandonment = []
         self.customer_queue = []
 
-    def reset(self):
+    def reset_day(self):
+        """
+        reset all variables for a new. Stats will be saved.
+
+        :return:
+        """
         pass
 
     def get_free_worker_random(self):
@@ -132,14 +142,42 @@ class System:
     def get_new_task(self):
         pass
 
-    def create_new_event(self, event_type: EventType, appearance_time: float, ):
+    def create_new_event(self, event_type: EventType, appearance_time: float, func: str, **kwargs: dict):
+        pass
+
+    def execute_event(self, event: Event):
+        if event.event_type is EventType.incoming_customer:
+            self.assign_new_customer(**event.kwargs)
+        elif event.event_type is EventType.abandoned_customer:
+            self.
+        elif event.event_type is EventType.worker_finished:
+            pass
+        else:
+            pass
 
     def assign_customer_to_worker(self, customer, day_time: float) -> None:
+        """
+        chooses a random worker and assigns the customer to this worker
+
+        :param customer:
+        :param day_time:
+        :return:
+        """
         worker, worker_id = self.get_free_worker_random()
         time = worker.serve_customer(customer)
         self.worker_serving_time[worker_id] = time + day_time
-        event = Event(event_type=EventType.worker_finished, appearance_time=time + day_time, list_index=worker_id)
+        event = Event(event_type=EventType.worker_finished, appearance_time=time + day_time)
         self.events.append(event)
+
+    def move_worker_to_free(self, worker_id: int):
+        """
+        the specified worker has finished his workerd and therefore will be moved to the available agents again
+
+        :return:
+        """
+        self.worker_serving_time[worker_id] = None
+        self.busy_worker_ids.remove(worker_id)
+        self.free_worker_ids.append(worker_id)
 
     def get_next_customer(self) -> (float, Customer):
         next_cust_index = int(np.argmin(self.processes_time_next_customer))
@@ -151,17 +189,29 @@ class System:
 
         return next_cust_time, next_cust
 
-    def assign_new_customer(self, day_time) -> None:
-        next_customer_time, next_customer = self.get_next_customer()
+    def remove_customer_from_queue(self):
+        """
+        removes a customer from the waiting queue since the abandoned
 
+        :return:
+        """
+        pass
+
+    def assign_new_customer(self, day_time: float, next_customer: Customer) -> None:
+        """
+        this function assigns a new customer to the queue or a worker if this is the next event
+
+        :param day_time:
+        :param next_customer:
+        :return:
+        """
         if self.is_worker_available() and len(self.customer_queue) == 0:
             self.assign_customer_to_worker(next_customer, day_time=day_time)
         else:
             self.customer_queue.append(next_customer)
             self.customer_queue_abandonment.append(next_customer.patience + day_time)
-            # TODO find something how to solve the list index problem here since the list willl be always popped
             event = Event(event_type=EventType.abandoned_customer,
-                          appearance_time=next_customer.patience + day_time, list_index=None)
+                          appearance_time=next_customer.patience + day_time, kwargs={})
             self.events.append(event)
             if self.is_worker_available():
                 customer = self.customer_queue.pop()
@@ -181,18 +231,11 @@ class System:
                     time, cust = self.processes[i].get_customer()
                     self.processes_next_customer[i] = cust
                     self.processes_time_next_customer[i] = time + day_time
-                    event = Event(event_type=EventType.incoming_customer, list_index=i, appearance_time=time + day_time)
+                    event = Event(event_type=EventType.incoming_customer, appearance_time=time + day_time, kwargs={"day_time": day_time})
                     self.events.append(event)
 
-
-
-
-
-
-            # TODO check for abandonment's
-
-
-
-
+                next_event = self.events.pop()
+                self.execute_event(next_event)
+                day_time = next_event.appearance_time
 
 
